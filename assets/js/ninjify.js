9d0b7ef55d230bdf7463dad90b082ee5d07866d3
Alexandre Leblanc <alexandreleblanc89@gmail.com>
2021-02-25 21:39:10 -0500

    parse response from api now working

---
+++ assets/js/ninjify.js
@@ -0,0 +1,28 @@
+function ninjify() {
+
+  var ying = $("#yingbuzz").val();
+  var yang = $("#yangbuzz").val();
+
+  if (ying == "Cheat" && yang == "Code") {
+    $("#ninjaname").val("True Warrior");
+  }
+  else if (ying == "Cheat" && yang != "Code") {
+    $("#ninjaname").val('"Cheat" is missing something ...');
+  }  
+  else if (ying != "Cheat" && yang == "Code") {
+    $("#ninjaname").val('"Code" is missing something ...');
+  }  
+  else {
+    $.ajax({
+        url: 'https://coding-challenge-tlm2021.herokuapp.com/ninjify' + "?x=" + $("#yingbuzz").val() + "," + $("#yangbuzz").val(),
+        contentType: "application/json",
+        dataType: 'json',
+        success: function(response) {
+            console.log(response);
+            var result = JSON.stringify(response);
+            var nickname = JSON.parse(result);
+            $('#ninjaname').val(nickname.message);
+        }
+    });
+  };
+};
