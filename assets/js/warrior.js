fc70a9d27a5e5253e6920c91ebb6450a1418de37
Alexandre Leblanc <alexandreleblanc89@gmail.com>
2021-02-26 03:41:00 -0500

    final

--- assets/js/warrior.js
+++ assets/js/warrior.js
@@ -14,5 +14,9 @@ function warrior() {
 
     if (keyone == "UP" && keytwo == "UP" && keythree == "DOWN" && keyfour == "DOWN" && keyfive == "LEFT" && keysix == "RIGHT" && keyseven == "LEFT" && keyeight == "RIGHT" && keynine == "B" && keyten == "A" && keyeleven == "START") {
         $("#konami").show();
-    };
+    }
+
+    // if (keyone == "1" && keytwo == "1" && keythree == "2" && keyfour == "2" && keyfive == "3" && keysix == "4" && keyseven == "3" && keyeight == "4" && keynine == "6" && keyten == "5" && keyeleven == "7") {
+    //     $("#konami").show();
+    // }
 };
